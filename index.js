19d00b43b437802f4bd522327a389ef05d8604c7
infoomics <sbx_94@hotmail.com>
2023-01-26 12:24:38 +0000

    added code for subclasses and methods

--- index.js
+++ index.js
@@ -13,21 +13,25 @@ class Pokemon {
     }
     return false;
   }
+
   isWeakto(pokemon) {
     if (this.type === "normal") {
       return false;
     }
     return true;
   }
+
   takeDamage(num) {
     this.hitPoints -= num;
   }
+
   useMove() {
     console.log(
       `Pokemon${this.name} used ${this.attackDamage} Pokemon${this.name}'s moves`
     );
     return this.attackDamage;
   }
+
   hasFainted() {
     if (this.hitPoints <= 0) {
       return true;
@@ -36,4 +40,57 @@ class Pokemon {
   }
 }
 
-module.exports = Pokemon;
+class FirePokemon extends Pokemon {
+    isEffectiveAgainst(pokemon) {
+        if (pokemon.type === "grass") {
+            return true;
+        }
+        return false;
+    }
+    
+    isWeakto(pokemon) {
+        if (pokemon.type === "water") {
+            return true;
+        }
+        return false;
+    }
+}
+
+class WaterPokemon extends Pokemon {
+    isEffectiveAgainst(pokemon) {
+        if (pokemon.type === "fire") {
+            return true;
+        }
+        return false;
+    }
+    
+    isWeakto(pokemon) {
+        if (pokemon.type === "grass") {
+            return true;
+        }
+        return false;
+    }
+}
+
+class GrassPokemon extends Pokemon {
+    isEffectiveAgainst(pokemon) {
+        if (pokemon.type === "water") {
+            return true;
+        }
+        return false;
+    }
+    
+    isWeakto(pokemon) {
+        if (pokemon.type === "fire") {
+            return true;
+        }
+        return false;
+    }
+}
+
+module.exports = [
+    Pokemon,
+    FirePokemon,
+    WaterPokemon,
+    GrassPokemon
+];
